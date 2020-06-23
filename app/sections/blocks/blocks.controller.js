b30016197f7c739064ef5b678d041c3317c2f240
Ihor Brazhnichenko <ihor.bra@gmail.com>
2020-06-23 05:49:31 +0300

    #3 Display transactions on a block page if raw json checkbox is unchecked

--- app/sections/blocks/blocks.controller.js
+++ app/sections/blocks/blocks.controller.js
@@ -10,12 +10,38 @@
         const path = $location.path();
         let name = $routeParams.name;
         if(name) {
-
+            
+            $scope.showRawData = false;
+            
             name = name.toUpperCase();
             if(path.includes("blocks")) {
 
                 networkService.getBlock(name, function (returnData) {
                     $scope.data = returnData;
+                    $scope.transactions = {};
+                    
+                    if($scope.data && $scope.data.transactions) {
+                        $scope.data.transactions.map((transaction) => {
+                            
+                            $scope.transactions[transaction.trx_id] = {
+                              data: undefined,
+                              operations: undefined,
+                            };
+                            
+                            networkService.getTransaction(transaction.trx_id, function (trxData) {
+                                $scope.transactions[transaction.trx_id] = {
+                                    data: trxData.meta,
+                                    operations: trxData.operations,
+                                }
+                            }).catch(() => {
+                                $scope.transactions[transaction.trx_id] = {
+                                    data: 'error',
+                                    operations: 'error',
+                                }
+                            });
+                        });
+                    }
+                    
                 });
             }
         }
