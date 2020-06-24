cf80565b5acdf6ac19802c9b046604d21c9a72bc
Stefan Schiessl <stefan.schiessl@blockchainprojectsbv.com>
2020-06-24 12:48:14 +0200

    add missing , and resort
    Signed-off-by: Stefan Schiessl <stefan.schiessl@blockchainprojectsbv.com>

--- html.js
+++ html.js
@@ -1,50 +1,32 @@
 'use strict';
 
 const HTML = [
-
     "sections/accounts/account.html",
     "sections/accounts/accounts.html",
-
     "sections/assets/asset.html",
     "sections/assets/assets.html",
-
     "sections/blocks/block.html",
     "sections/blocks/blocks.html",
-
     "sections/committee_members/committee_members.html",
-
     "sections/dashboard/dashboard.html",
-
     "sections/fees/fees.html",
-
-    "sections/layout/header.html",
     "sections/layout/footer.html",
-
+    "sections/layout/header.html",
     "sections/markets/market.html",
     "sections/markets/markets.html",
-
+    "sections/network_dropdown/network_dropdown.html",
     "sections/objects/object.html",
-
     "sections/operations/operation.html",
     "sections/operations/operations.html",
-
     "sections/proxies/proxies.html",
-
     "sections/search/search.html",
-
     "sections/txs/tx.html",
     "sections/txs/txs.html",
-
     "sections/voting/voting.html",
-
     "sections/witnesses/witnesses.html",
-
     "sections/workers/workers.html",
 
-    "sections/network_dropdown/network_dropdown.html"
-
     "ui/responsive-table.html",
-
 ];
 
 const CSS = [
