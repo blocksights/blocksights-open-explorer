52d0ff50b627f160d21b9ce50beb68e6cdfff8c9
Stefan Schiessl <stefan.schiessl@blockchainprojectsbv.com>
2020-08-05 15:50:13 +0200

    update branding (main, test, local)
    Signed-off-by: Stefan Schiessl <stefan.schiessl@blockchainprojectsbv.com>

--- app/branding.js
+++ app/branding.js
@@ -7,16 +7,22 @@ export function getAvailableEndpoints() {
         {
             key: "mainnet",
             translate: 'Mainnet',
-            url: 'http://127.0.0.1:5000/openexplorer',
+            url: 'https://api.bitshares.ws/openexplorer',
             chainId: '4018d7844c78f6a6c41c6a552b898022310fc5dec06da467ee7905a8dad512c8',
             isDefault: true,
         },
         {
             key: "testnet",
             translate: 'Testnet',
-            url: 'http://127.0.0.1:5001/openexplorer',
+            url: 'https://api.testnet.bitshares.ws/openexplorer',
             chainId: '39f5e2ede1f8bc1a3a54a7914414e3779e33193f1f5693510e73cb7a87617447'
         },
+        {
+            key: "local",
+            translate: 'Local',
+            url: 'http://localhost:5000/openexplorer',
+            chainId: '4018d7844c78f6a6c41c6a552b898022310fc5dec06da467ee7905a8dad512c8'
+        }
     ];
 }
 
