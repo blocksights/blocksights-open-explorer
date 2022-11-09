2e0f1fb0fde768ca5fad8427eb2270fec1519c1b
Ihor Brazhnichenko <ihor.bra@gmail.com>
2022-11-09 09:20:44 +0200

    #23 Replace operations view list by operations-table

--- app/sections/operations/operations.controller.js
+++ app/sections/operations/operations.controller.js
@@ -2,9 +2,9 @@
     'use strict';
 
     angular.module('app.operations')
-        .controller('operationsCtrl', ['$scope', 'Notify', '$filter', '$routeParams', '$location', 'networkService', operationsCtrl]);
+        .controller('operationsCtrl', ['$scope', '$routeParams', '$location', 'networkService', operationsCtrl]);
 
-    function operationsCtrl($scope, Notify, $filter, $routeParams, $location, networkService) {
+    function operationsCtrl($scope, $routeParams, $location, networkService) {
 
         const path = $location.path();
         const name = $routeParams.name;
@@ -19,56 +19,6 @@
         else {
             if (path === "/operations") {
 
-                $scope.operationsColumns = [
-                    {
-                        title: $filter('translate')('Operation'),
-                        index: 'operation_text',
-                    },
-                    {
-                        title: $filter('translate')('ID'),
-                        index: 'operation_id',
-                    },
-                    {
-                        title: $filter('translate')('Date and time'),
-                        index: 'time',
-                        hidden: ['xs']
-                    },
-                    {
-                        title: $filter('translate')('Block'),
-                        index: 'block_num',
-                        hidden: ['xs', 'sm']
-                    },
-                    {
-                        title: $filter('translate')('Type'),
-                        index: 'type',
-                        hidden: ['xs', 'sm', 'md']
-                    }
-                ];
-                
-                $scope.userOpenedFirstPageTime = null;
-
-                $scope.select = function (page_operations) {
-                    const page = page_operations - 1;
-                    const limit = 50;
-                    const from = page * limit;
-                    
-                    if(page_operations === 1 || !$scope.userOpenedFirstPageTime) { // if user switches back from page Y (Y>1) to page 1 we need to fetch new transactions and update time range
-                        $scope.userOpenedFirstPageTime = new Date();
-                    }
-                    
-                    const date_to = $scope.userOpenedFirstPageTime.toISOString();
-
-                    $scope.operationsLoading = true;
-                    networkService.getLastOperations(limit, from, date_to, function (returnData) {
-                        $scope.operationsLoading = false;
-                        $scope.operations = returnData;
-                        $scope.currentPage = page_operations;
-                        $scope.total_ops = 10000;
-                    }).catch(err => {
-                        $scope.operationsLoadingError = true;
-                    });
-                };
-                $scope.select(1);
             }
         }
     }
