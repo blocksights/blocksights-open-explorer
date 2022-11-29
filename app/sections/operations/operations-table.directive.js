47187557be06a51e7f131b37c264306e1bd489e9
Ihor Brazhnichenko <ihor.bra@gmail.com>
2022-11-29 19:40:29 +0200

    #23 Add filters for responsive-table and use operations-table like an example

--- app/sections/operations/operations-table.directive.js
+++ app/sections/operations/operations-table.directive.js
@@ -88,7 +88,7 @@ Use this field show / hide user filters
                 if(!name)
                     return false;
                 return {
-                    id: key,
+                    value: key,
                     name,
                 }
             }).filter((item) => !!item);
@@ -119,12 +119,81 @@ Use this field show / hide user filters
             // see the issue: https://github.com/blocksights/blocksights-open-explorer/issues/15
             $scope.userOpenedFirstPageTime = null;
             
+            const filters = [
+                $scope.filterByAssetIdEnabled !== false ? {
+                    width: '150px',
+                    defaultValue: '',
+                    placeholder: 'Search by asset',
+                    onChange: (value) => {
+                        $scope.filters.assetIdOrName = value;
+                        $scope.select(1, true);
+                    },
+                    modelOptions: {
+                        debounce: 500,
+                        getterSetter: true
+                    }
+                } : null,
+                $scope.filterByAccountIdEnabled !== false ? {
+                    width: '150px',
+                    defaultValue: '',
+                    placeholder: 'Search by account',
+                    onChange: (value) => {
+                        $scope.filters.accountIdOrName = value;
+                        $scope.select(1, true);
+                    },
+                    modelOptions: {
+                        debounce: 500,
+                        getterSetter: true
+                    }
+                } : null,
+            ].filter((item) => !!item);
+            
             $scope.operationsColumns = $scope.columns || [
-                OperationsService.columns.OPERATION_TEXT,
-                OperationsService.columns.OPERATION_ID,
-                OperationsService.columns.DATETIME,
-                OperationsService.columns.BLOCK,
-                OperationsService.columns.TYPE,
+                {
+                    title: $filter('translate')('Operation'),
+                    index: 'operation_text',
+                    onFilterCancel: () => {
+                        $scope.filters.assetIdOrName = undefined;
+                        $scope.filters.accountIdOrName = undefined;
+                        $scope.select(1, true)
+                    },
+                    filter: filters.length ? filters : undefined
+                },
+                {
+                    title: $filter('translate')('ID'),
+                    index: 'operation_id',
+                },
+                {
+                    title: $filter('translate')('Date and time'),
+                    index: 'time',
+                    hidden: ['xs']
+                },
+                {
+                    title: $filter('translate')('Block'),
+                    index: 'block_num',
+                    hidden: ['xs', 'sm']
+                },
+                {
+                    title: $filter('translate')('Type'),
+                    index: 'type',
+                    hidden: ['xs', 'sm', 'md'],
+                    onFilterCancel: () => {
+                        $scope.filters.operationType = '-1';
+                        $scope.select(1, true)
+                    },
+                    filter: $scope.filterByOperationTypeEnabled !== false ? [
+                        {
+                            width: '300px',
+                            placeholder: 'Filter by operation id',
+                            defaultValue: '-1',
+                            onChange: (value) => {
+                                $scope.filters.operationType = value;
+                                $scope.select(1, true);
+                            },
+                            options: $scope.operationTypes
+                        }
+                    ] : undefined
+                }
             ];
             
             $scope.loadMore = () => {
