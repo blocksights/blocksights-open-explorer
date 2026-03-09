a33c7b33eb186c992a2e06f0f60fee39d9f35871
sschiessl-bcp <toleantech@gmail.com>
2026-03-09 22:06:23 +0100

    fix: update url

--- app/branding.js
+++ app/branding.js
@@ -7,14 +7,14 @@ export function getAvailableEndpoints() {
         {
             key: "mainnet",
             translate: 'Mainnet',
-            url: 'https://api.bitshares.ws/openexplorer',
+            url: 'https://api.blocksights.info/openexplorer',
             chainId: '4018d7844c78f6a6c41c6a552b898022310fc5dec06da467ee7905a8dad512c8',
             isDefault: true,
         },
         {
             key: "testnet",
             translate: 'Testnet',
-            url: 'https://api.testnet.bitshares.ws/openexplorer',
+            url: 'https://api.testnet.blocksights.info//openexplorer',
             chainId: '39f5e2ede1f8bc1a3a54a7914414e3779e33193f1f5693510e73cb7a87617447'
         },
         {
